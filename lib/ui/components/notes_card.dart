4171b3c26836079a3129a986a27f0dbced5bb354
matheusantonio <matheus.cardoso@cobli.co>
2024-09-02 17:57:52 -0300

    Added card list

--- lib/ui/components/notes_card.dart
+++ lib/ui/components/notes_card.dart
@@ -1,30 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:notas/domain/note.dart';
 
 class NotesCard extends StatelessWidget {
-  const NotesCard({super.key});
+  const NotesCard({super.key, required this.note});
+
+  final Note note;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black,
-            blurRadius: 20.0,
-          )
-        ]
-      ),
-      child: const Card(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              ListTile(
-                title: Text('Cartão 1'),
-                subtitle: Text('Subtítulo 1'),
-              )
-            ],
-          )
-      )
+    return Padding(
+      padding: const EdgeInsets.only(top: 8),
+      child: SizedBox(
+          /*decoration: const BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black,
+              blurRadius: 20.0,
+            )
+          ]
+        ),*/
+          height: 66,
+          child: Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Text(note.title,
+                        textAlign: TextAlign.left,
+                        style: GoogleFonts.roboto(
+                            fontSize: 16, fontWeight: FontWeight.w700)),
+                    Text(note.content,
+                        textAlign: TextAlign.left,
+                        style:
+                            GoogleFonts.roboto(fontSize: 14, color: Colors.grey)),
+                  ],
+                ),
+              ))),
     );
   }
 }
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:notas/ui/components/notes_app_bar.dart';
 import 'package:notas/ui/components/notes_empty_state.dart';
 import 'package:notas/ui/components/notes_floating_button.dart';
+import 'package:notas/ui/components/notes_list.dart';
 
+import '../../domain/note.dart';
 import '../components/notes_card.dart';
 
 class HomePage extends StatelessWidget {
@@ -14,12 +16,15 @@ class HomePage extends StatelessWidget {
     final creatingNote = false;
     final icon = creatingNote ? Icons.add : Icons.save;
 
-    final notes = ['asd'];
+    final notes = [
+      new Note(title: "Título 1", content: "Descrição 1"),
+      new Note(title: "Título 2", content: "Descrição 2")
+    ];
 
     return Scaffold(
       appBar: const NotesAppBar(title: 'Notas'),
       floatingActionButton: NotesFloatingButton(buttonIcon: icon),
-      body: notes.isEmpty ? const Center(child: NotesEmptyState()) : const NotesCard()
+      body: notes.isEmpty ? const Center(child: NotesEmptyState()) : NotesList(notes: notes)
     );
   }
 }
