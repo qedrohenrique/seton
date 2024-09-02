bce4a9042066a2a36db83095a3232199069cde84
qedrohenrique <pedro.005henr@gmail.com>
2024-09-02 15:41:11 -0300

    feat: use components and colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notas/ui/pages/home_page.dart';
 import 'package:notas/ui/theme/custom_theme.dart';
+import 'package:notas/util/colors.dart';
 
 void main() {
   runApp(NotesApp());
@@ -13,14 +14,17 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(94, 52, 193, 1)),
+        colorScheme: ColorScheme.fromSeed(seedColor: CustomColors.purple),
         useMaterial3: false,
         extensions: const [
           CustomTheme(
             backgroundColor: Colors.white,
             textColor: Colors.black,
           ),
-        ]
+        ],
+        // floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        //   backgroundColor: CustomColors.purple
+        // ),
       ),
       home: HomePage(),
     );
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:notas/ui/components/app_bar.dart';
+import 'package:notas/ui/components/notes_app_bar.dart';
+import 'package:notas/ui/components/notes_empty_state.dart';
+import 'package:notas/ui/components/notes_floating_button.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: NotesAppBar(title: 'Notas'),
+
+    final creatingNote = false;
+    final icon = creatingNote ? Icons.add : Icons.save;
+
+    return Scaffold(
+      appBar: const NotesAppBar(title: 'Notas'),
+      floatingActionButton: NotesFloatingButton(buttonIcon: icon),
+      body: const Center(child: NotesEmptyState())
     );
   }
 }
